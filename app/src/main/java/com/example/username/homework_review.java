d43d8c1bbf0abaf64ffd5edde2f40a04906aa846
Rahul prasad <prasadrahul9719@gmail.com>
2020-11-07 18:31:26 +0530

    3 fragments added

---
+++ app/src/main/java/com/example/username/homework_review.java
@@ -0,0 +1,14 @@
+package com.example.username;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class homework_review extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homework_review);
+    }
+}
---
+++ app/src/main/java/com/example/username/homewrk_edit.java
@@ -0,0 +1,14 @@
+package com.example.username;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class homewrk_edit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homewrk_edit);
+    }
+}
