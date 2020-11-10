c16e2bbc5803a0a7313e21343473fe2c3d890a2d
Rahul prasad <prasadrahul9719@gmail.com>
2020-11-10 16:09:03 +0530

    answer_check page added

---
+++ app/src/main/java/com/example/username/answer_check.java
@@ -0,0 +1,14 @@
+package com.example.username;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class answer_check extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_answer_check);
+    }
+}
---
+++ app/src/main/java/com/example/username/student_record_attendance.java
@@ -0,0 +1,14 @@
+package com.example.username;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class student_record_attendance extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_record_attendance);
+    }
+}
